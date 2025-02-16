03829d8415fbe644181456f830aae1e15ef37a71
Bakayarouz <56614842+Bakayarouz@users.noreply.github.com>
2025-02-16 20:27:51 +0700

    Create SpectatorClickListener.java

---
+++ src/main/java/com/yourname/spectatorclicklistener/SpectatorClickListener.java
@@ -0,0 +1,42 @@
+package com.yourname.spectatorclicklistener;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerInteractEvent.Action;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpectatorClickListener extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Register events when the plugin starts
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("SpectatorClickListener has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SpectatorClickListener has been disabled.");
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        // Check if the player is in Spectator mode
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+
+            // Handle left-click action
+            if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+                player.sendMessage("You left-clicked in Spectator mode!");
+            } 
+            // Handle right-click action
+            else if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                player.sendMessage("You right-clicked in Spectator mode!");
+            }
+        }
+    }
+}

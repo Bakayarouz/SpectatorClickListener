5c705e4665ec8fc5a32f7086a9fe3d48c81451ab
Bakayarouz <56614842+Bakayarouz@users.noreply.github.com>
2025-02-16 20:46:06 +0700

    Update SpectatorClickListener.java

--- src/main/java/com/yourname/spectatorclicklistener/SpectatorClickListener.java
+++ src/main/java/com/yourname/spectatorclicklistener/SpectatorClickListener.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerInteractEvent.Action; // <-- This import is necessary
+import org.bukkit.event.player.PlayerInteractEvent.Action;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpectatorClickListener extends JavaPlugin implements Listener {
